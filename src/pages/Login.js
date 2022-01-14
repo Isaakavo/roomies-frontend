67f3e41ed4f0b5902d240ed4dbf89c0a462f14c4
Isaakavo <isaakhaas96@gmail.com>
2022-01-13 21:23:27 -0600

    feat: creation of login page and logic implemented

---
+++ src/pages/Login.js
@@ -0,0 +1,71 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
+
+import { ContexDefault } from '../App';
+
+//TODO
+//Investigate more about http only flag and try to implement it
+//use react context (or redux) to store the JWT returned from the service
+// implement styles, first using styled components, then evaluate
+//the option of a css framework
+
+const Login = () => {
+  const [value, setValue] = useState({ username: '', password: '' });
+
+  const { putJwt, putTasks, host } = useContext(ContexDefault);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const dataToSend = {
+      username: value.username,
+      password: value.password,
+    };
+    const response = await axios.post(host + 'login', dataToSend);
+    putJwt(response.data.token);
+    if (response.status === 200) {
+      const { token, token_type } = response.data;
+      const tokenHeader = token_type + ' ' + token;
+      axios.defaults.headers.common['Authorization'] = tokenHeader;
+      const tasksResponse = await axios.get(host + 'tasks');
+
+      putTasks({
+        tasks: tasksResponse.data.tasks,
+        count: tasksResponse.data.count,
+      });
+
+      navigate('/dashboard');
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <span>Usuario</span>
+        <input
+          type='text'
+          name='username'
+          value={value.username}
+          onChange={handleChange}
+        />
+        <br />
+        <span>Contraseña</span>
+        <input
+          type='password'
+          name='password'
+          value={value.password}
+          onChange={handleChange}
+        />
+        <input type='submit' value='Iniciar sesión' />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
